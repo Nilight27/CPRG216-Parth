02ed0a37b0264c6abe8a06044fd1b531fad3d57b
Nilight27 <Carparths023@gmail.com>
2025-09-22 17:22:07 -0600

    it works

--- Test.py
+++ Test.py
@@ -1,5 +1,11 @@
-print("Hello World")
-x = 1
+y = ['AB','BC','MC','BMW']
 
-if (x == 0):
-    print('l')
+
+
+province = input('what is ur province')
+province = province.upper()
+
+if province in y[0:2]:
+    gst = 0.05
+if province in y[4]:
+    gst = 0.13
